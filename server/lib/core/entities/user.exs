e4ef118c79fe2c04490c5ab4f62253b7d3969b50
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-14 21:31:35 +0100

    feat: update user model

--- server/lib/core/entities/user.exs
+++ server/lib/core/entities/user.exs
@@ -1,10 +1,6 @@
 defmodule User do
-  use Ecto.Model
+  use Ecto.Schema
 
-  # see the note below for explanation of that line
-  @primary_key {:id, :binary_id, autogenerate: true}
-
-  # weather is the MongoDB collection name
   schema "user" do
     field :username,    :string
     field :email, :string
